0aab317066678b8121209be98ac6eb57a92fed09
Yulia <ne3nakomka8@gmail.com>
2021-01-04 00:29:48 +0300

    adding client

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+
+import React, {useContext} from 'react'
+import {NavLink, useHistory} from 'react-router-dom'
+import {AuthContext} from '../context/AuthContext'
+
+export const Navbar = () => {
+  const history = useHistory()
+  const auth = useContext(AuthContext)
+
+  const logoutHandler = event => {
+    event.preventDefault()
+    auth.logout()
+    history.push('/')
+  }
+
+  return (
+    <nav>
+      <div className="nav-wrapper teal" style={{ padding: '0 2rem' }}>
+        <span className="brand-logo">Cut the link</span>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><NavLink to="/create">Create</NavLink></li>
+          <li><NavLink to="/links">History</NavLink></li>
+          <li><a href="/" onClick={logoutHandler}>Log out</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
