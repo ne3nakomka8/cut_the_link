380e5ae0c56ce1d987081cbf4b4c28afb8b3b973
Yulia <ne3nakomka8@gmail.com>
2021-01-05 22:45:18 +0300

    without folder build

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -4,8 +4,6 @@ import React from 'react'
 export const LinkCard = ({link}) => {
     return (
         <>
-        
-
         <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
         <p>From: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
         <p>Statystic about clicks: <strong> {link.clicks}</strong></p>
