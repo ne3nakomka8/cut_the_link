79bba79e6d3eef7ec080c0e601e318501a070b98
Yulia <ne3nakomka8@gmail.com>
2021-01-05 22:16:25 +0300

    fixing link page, logo

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 export const LinkCard = ({link}) => {
     return (
         <>
-        <h2> Link</h2>
+        
 
         <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
         <p>From: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
