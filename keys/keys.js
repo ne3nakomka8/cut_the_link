e6aaf98a87dec27315825112681da85a46e29c6e
Yulia <ne3nakomka8@gmail.com>
2021-01-08 22:30:06 +0300

    added sendgrid

--- keys/keys.js
+++ keys/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
     SENDGRID_API_KEY: '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',
     EMAIL_FROM: 'ne3nakomka8@gmail.com',
-    BASE_URL: 'http://localhost:5000'
-}
+    BASE_URL: 'https://cut-your-link.herokuapp.com'
